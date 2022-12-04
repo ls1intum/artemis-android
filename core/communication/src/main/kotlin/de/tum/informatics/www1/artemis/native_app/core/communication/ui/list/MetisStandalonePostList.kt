c0a8f342add0e599b822ac3ece0515cb4657e689
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2022-12-04 17:31:17 +0100

    Added loading and retry ui to communication.

--- core/communication/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/communication/ui/list/MetisStandalonePostList.kt
+++ core/communication/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/communication/ui/list/MetisStandalonePostList.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import de.tum.informatics.www1.artemis.native_app.core.communication.R
 import de.tum.informatics.www1.artemis.native_app.core.communication.ui.PostItem
 import de.tum.informatics.www1.artemis.native_app.core.communication.ui.PostItemViewType
 import de.tum.informatics.www1.artemis.native_app.core.communication.ui.getEmojiForEmojiId
@@ -23,36 +28,107 @@ internal fun MetisStandalonePostList(
 ) {
     val posts: LazyPagingItems<Post> = viewModel.postPagingData.collectAsLazyPagingItems()
 
-    Column(modifier = modifier) {
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(8.dp),
-            contentPadding = PaddingValues(top = 8.dp)
-        ) {
-            items(posts, key = { it.clientPostId }) { post ->
-                PostItem(
-                    modifier = Modifier.fillMaxWidth(),
-                    post = post,
-                    postItemViewType = PostItemViewType.StandaloneListItem(
-                        answerPosts = post?.answerPostings.orEmpty(),
-                        onClickReply = {
-                            if (post != null) {
-                                onClickReply(post.clientPostId)
-                            }
-                        },
-                        onClickViewReplies = {
-                            if (post != null) {
-                                onClickViewReplies(post.clientPostId)
-                            }
-                        }
-                    ),
-                    getUnicodeForEmojiId = {
-                        val emoji = getEmojiForEmojiId(emojiId = it)
-                        println(emoji)
-                        emoji
-                    }
+    Box(modifier = modifier) {
+        val informationModifier = Modifier
+            .align(Alignment.Center)
+            .padding(16.dp)
+        when {
+            posts.itemCount == 0 &&
+                    (posts.loadState.refresh.endOfPaginationReached || posts.loadState.append.endOfPaginationReached) -> {
+                Text(
+                    text = stringResource(id = R.string.metis_post_list_empty),
+                    modifier = informationModifier,
+                    style = MaterialTheme.typography.bodyLarge,
+                    textAlign = TextAlign.Center
                 )
             }
+
+            posts.loadState.refresh is LoadState.Loading -> {
+                Column(
+                    modifier = informationModifier,
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.metis_post_list_loading),
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center
+                    )
+
+                    LinearProgressIndicator(
+                        modifier = Modifier.fillMaxWidth(0.4f)
+                    )
+                }
+            }
+
+            posts.loadState.refresh is LoadState.Error -> {
+                PageStateError(modifier = informationModifier, retry = posts::retry)
+            }
+
+            else -> {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(top = 8.dp)
+                ) {
+                    items(posts, key = { it.clientPostId }) { post ->
+                        PostItem(
+                            modifier = Modifier.fillMaxWidth(),
+                            post = post,
+                            postItemViewType = PostItemViewType.StandaloneListItem(
+                                answerPosts = post?.answerPostings.orEmpty(),
+                                onClickReply = {
+                                    if (post != null) {
+                                        onClickReply(post.clientPostId)
+                                    }
+                                },
+                                onClickViewReplies = {
+                                    if (post != null) {
+                                        onClickViewReplies(post.clientPostId)
+                                    }
+                                }
+                            ),
+                            getUnicodeForEmojiId = {
+                                val emoji = getEmojiForEmojiId(emojiId = it)
+                                println(emoji)
+                                emoji
+                            }
+                        )
+                    }
+
+                    if (posts.loadState.append is LoadState.Loading) {
+                        item {
+                            LinearProgressIndicator(modifier = Modifier.fillMaxWidth(0.4f))
+                        }
+                    }
+
+                    if (posts.loadState.append is LoadState.Error) {
+                        item {
+                            PageStateError(modifier = Modifier.fillMaxWidth(), retry = posts::retry)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun PageStateError(
+    modifier: Modifier,
+    retry: () -> Unit
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.metis_post_list_error),
+            style = MaterialTheme.typography.bodyLarge,
+            textAlign = TextAlign.Center
+        )
+
+        Button(onClick = retry) {
+            Text(text = stringResource(id = R.string.metis_post_list_error_try_again))
         }
     }
 }
