2639bc7a3f5c876be78a55fd1513ca4651cc96ce
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2022-12-25 13:56:04 +0100

    Bump to version code 4 and name 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "de.tum.informatics.www1.artemis.native_app.android"
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.2.0"
     }
     packagingOptions {
         resources {
