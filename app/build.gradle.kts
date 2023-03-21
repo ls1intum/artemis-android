c4422ffff7812063074157a36766a1642f648706
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2023-03-21 14:13:16 +0100

    Update docu.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,9 @@
 import java.util.Properties
 import java.io.FileInputStream
 
-// Create a variable called keystorePropertiesFile, and initialize it to your
-// keystore.properties file, in the rootProject folder.
+// https://developer.android.com/studio/publish/app-signing#secure-shared-keystore
 val keystorePropertiesFile = rootProject.file("keystore.properties")
-
-// Initialize a new Properties() object called keystoreProperties.
 val keystoreProperties = Properties()
-
-// Load your keystore.properties file into the keystoreProperties object.
 keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 plugins {
