c887310e2cda8e118f2a685126e4d663272fae39
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2023-04-07 14:52:04 +0200

    Remove HM instance and do not show webview for quiz exercises.

--- feature/exercise_view/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/exercise_view/home/overview/ExerciseOverviewTab.kt
+++ feature/exercise_view/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/exercise_view/home/overview/ExerciseOverviewTab.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.unit.dp
 import com.google.accompanist.web.LoadingState
 import com.google.accompanist.web.WebViewState
 import de.tum.informatics.www1.artemis.native_app.core.model.exercise.Exercise
+import de.tum.informatics.www1.artemis.native_app.core.model.exercise.QuizExercise
 import de.tum.informatics.www1.artemis.native_app.core.ui.exercise.ExerciseActions
 import de.tum.informatics.www1.artemis.native_app.feature.exercise_view.ProblemStatementWebView
 
@@ -33,30 +34,32 @@ internal fun ExerciseOverviewTab(
             actions = actions
         )
 
-        Box(
-            modifier = if (webViewState?.isLoading != true) Modifier.fillMaxWidth() else
-                Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-        ) {
-            if (webViewState != null) {
-                ProblemStatementWebView(
-                    modifier = Modifier.fillMaxWidth(),
-                    webViewState = webViewState,
-                    webView = webView,
-                    setWebView = setWebView
-                )
-            }
-
-            val loadingState = webViewState?.loadingState
-            if (loadingState is LoadingState.Loading) {
-                LinearProgressIndicator(
-                    modifier = Modifier
+        if (exercise !is QuizExercise) {
+            Box(
+                modifier = if (webViewState?.isLoading != true) Modifier.fillMaxWidth() else
+                    Modifier
                         .fillMaxWidth()
-                        .padding(8.dp)
-                        .align(Alignment.Center),
-                    progress = loadingState.progress
-                )
+                        .height(200.dp)
+            ) {
+                if (webViewState != null) {
+                    ProblemStatementWebView(
+                        modifier = Modifier.fillMaxWidth(),
+                        webViewState = webViewState,
+                        webView = webView,
+                        setWebView = setWebView
+                    )
+                }
+
+                val loadingState = webViewState?.loadingState
+                if (loadingState is LoadingState.Loading) {
+                    LinearProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                            .align(Alignment.Center),
+                        progress = loadingState.progress
+                    )
+                }
             }
         }
     }
