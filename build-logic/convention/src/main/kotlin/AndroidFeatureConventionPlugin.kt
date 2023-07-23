da9de8b16ccd8d0c30b1634771bcb4a2ede70ea8
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2023-07-23 13:29:35 +0200

    Gradle cache optimization.

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -93,8 +93,6 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
 
                     reports.junitXml.required.set(true)
                     reports.junitXml.outputLocation.set(rootProject.rootDir.resolve("test-outputs/${project.name}/$name/"))
-
-                    maxParallelForks = (Runtime.getRuntime().availableProcessors() / 2).takeIf { it > 0 } ?: 1
                 }
             }
         }
