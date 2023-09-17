b584c5c09a7aa8bdf5431f01d81796b828843190
Tim Ortel <100865202+TimOrtel@users.noreply.github.com>
2023-09-17 13:55:43 +0200

    Update instances.

--- core/datastore/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/datastore/defaults/ArtemisInstances.kt
+++ core/datastore/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/datastore/defaults/ArtemisInstances.kt
@@ -25,19 +25,19 @@ object ArtemisInstances {
     )
 
     private val TumTs0 = ArtemisInstance(
-        host = "artemis-test0.artemis.cit.tum.de",
+        host = "artemis-test0.artemis.in.tum.de",
         name = R.string.artemis_instance_tum_test_server_0,
         type = ArtemisInstance.Type.TEST
     )
 
     private val TumTs1 = ArtemisInstance(
-        host = "artemis-test1.artemis.cit.tum.de",
+        host = "artemis-test1.artemis.in.tum.de",
         name = R.string.artemis_instance_tum_test_server_1,
         type = ArtemisInstance.Type.TEST
     )
 
     private val TumTs2 = ArtemisInstance(
-        host = "artemis-test2.cit.tum.de",
+        host = "artemis-test2.artemis.in.tum.de",
         name = R.string.artemis_instance_tum_test_server_2,
         type = ArtemisInstance.Type.TEST
     )
