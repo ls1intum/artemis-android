26a4d672ac476ad39eb517bbf3c9535eed55dbeb
Julian Waluschyk <juwalu02@gmail.com>
2024-11-05 13:38:57 +0100

    refactored Markdown transformer to prevent exercise and lecture links from being opened in browser

--- core/common/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/common/markdown/PostArtemisMarkdownTransformer.kt
+++ core/common/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/common/markdown/PostArtemisMarkdownTransformer.kt
@@ -2,7 +2,7 @@ package de.tum.informatics.www1.artemis.native_app.core.common.markdown
 
 class PostArtemisMarkdownTransformer(val serverUrl: String, val courseId: Long) : ArtemisMarkdownTransformer() {
     override fun transformExerciseMarkdown(title: String, url: String): String {
-        return "[$title]($serverUrl$url)"
+        return "[$title](artemis:/$url)"
     }
 
     override fun transformUserMentionMarkdown(text: String, fullName: String, userName: String): String = "[@$fullName](artemis://courses/$courseId/messages?username=$userName)"
