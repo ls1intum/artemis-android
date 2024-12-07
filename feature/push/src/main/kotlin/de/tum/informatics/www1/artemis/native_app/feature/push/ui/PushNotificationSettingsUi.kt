b6da21f0b2bb311b2ea097177eeca984cd52d4e4
Julian Waluschyk <juwalu02@gmail.com>
2024-12-07 15:11:19 +0100

    changed ReceivePushNotificationsSwitch design and added card layout

--- feature/push/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/push/ui/PushNotificationSettingsUi.kt
+++ feature/push/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/push/ui/PushNotificationSettingsUi.kt
@@ -3,11 +3,14 @@ package de.tum.informatics.www1.artemis.native_app.feature.push.ui
 import android.Manifest
 import android.os.Build
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -19,7 +22,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
@@ -115,58 +117,72 @@ private fun ReceivePushNotificationsSwitch(
         }
     }
 
-    Column(modifier = modifier) {
-        var isInfoTextExpanded: Boolean by rememberSaveable { mutableStateOf(false) }
-        var hasOverflow: Boolean by rememberSaveable { mutableStateOf(false) }
-
-        Text(
-            text = stringResource(id = R.string.push_notification_settings_receive_information),
-            style = MaterialTheme.typography.bodyMedium,
-            maxLines = if (isInfoTextExpanded) Int.MAX_VALUE else 2,
-            overflow = TextOverflow.Ellipsis,
-            onTextLayout = { result ->
-                hasOverflow = result.hasVisualOverflow
-            }
-        )
-
-        if (hasOverflow) {
-            ClickableText(
-                onClick = { isInfoTextExpanded = true },
-                text = AnnotatedString(stringResource(id = R.string.push_notification_settings_receive_information_expand)),
-                style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.linkTextColor)
-            )
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
+    Card(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Column(
+            modifier = modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Text(
-                modifier = Modifier.weight(1f),
-                text = stringResource(id = R.string.push_notification_settings_receive_label),
-                style = MaterialTheme.typography.bodyLarge
-            )
+            var isInfoTextExpanded: Boolean by rememberSaveable { mutableStateOf(false) }
+            var hasOverflow: Boolean by rememberSaveable { mutableStateOf(false) }
 
-            Switch(
-                checked = isChecked,
-                onCheckedChange = { isChecked ->
-                    // Handle the cases where we need to ask thew user for permission
-                    val canShowPermissions =
-                        notificationPermissionState == null || notificationPermissionState.status.isGranted
-                    if (isChecked) {
-                        if (canShowPermissions) {
-                            onCheckedChange(true)
-                        } else {
-                            // When the user grants the permission, automatically enable notifications
-                            enableOnPermissionGranted = true
+            Column {
+                Text(
+                    text = stringResource(id = R.string.push_notification_settings_receive_information),
+                    style = MaterialTheme.typography.labelMedium,
+                    maxLines = if (isInfoTextExpanded) Int.MAX_VALUE else 2,
+                    overflow = TextOverflow.Ellipsis,
+                    onTextLayout = { result ->
+                        hasOverflow = result.hasVisualOverflow
+                    }
+                )
 
-                            // Ask for the permission
-                            notificationPermissionState?.launchPermissionRequest()
-                        }
-                    } else onCheckedChange(false)
+                if (hasOverflow) {
+                    Box(
+                        Modifier.clickable { isInfoTextExpanded = true }
+                    ) {
+                        Text(
+                            text = AnnotatedString(stringResource(id = R.string.push_notification_settings_receive_information_expand)),
+                            style = MaterialTheme.typography.labelMedium.copy(color = MaterialTheme.colorScheme.linkTextColor)
+                        )
+                    }
                 }
-            )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 10.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Text(
+                    modifier = Modifier.weight(1f),
+                    text = stringResource(id = R.string.push_notification_settings_receive_label),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+
+                Switch(
+                    checked = isChecked,
+                    onCheckedChange = { isChecked ->
+                        // Handle the cases where we need to ask thew user for permission
+                        val canShowPermissions =
+                            notificationPermissionState == null || notificationPermissionState.status.isGranted
+                        if (isChecked) {
+                            if (canShowPermissions) {
+                                onCheckedChange(true)
+                            } else {
+                                // When the user grants the permission, automatically enable notifications
+                                enableOnPermissionGranted = true
+
+                                // Ask for the permission
+                                notificationPermissionState?.launchPermissionRequest()
+                            }
+                        } else onCheckedChange(false)
+                    }
+                )
+            }
         }
     }
 }
