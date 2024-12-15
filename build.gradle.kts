cebeef67d92b01f444c83a869d8aaf242bc5a867
Martin Felber <felber.martin3@gmail.com>
2024-12-15 14:03:00 +0100

    Reordering plugins; add android.library as root plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,14 @@ buildscript {
 
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.google.firebase.appdistribution) apply false
+    alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.google.services) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.google.services) apply false
-    alias(libs.plugins.google.firebase.appdistribution) apply false
     alias(libs.plugins.kover) apply true
-    alias(libs.plugins.google.ksp) apply false
-    alias(libs.plugins.compose.compiler) apply false
 }
 
 tasks.register("clean", Delete::class) {
