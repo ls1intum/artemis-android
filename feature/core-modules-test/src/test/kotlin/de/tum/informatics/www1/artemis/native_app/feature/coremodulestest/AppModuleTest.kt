f121f9e5e286cd74f02d30660e61cbd84ef8abcd
Martin Felber <felber.martin3@gmail.com>
2024-12-20 08:07:00 +0100

    Using new verify API

---
+++ feature/core-modules-test/src/test/kotlin/de/tum/informatics/www1/artemis/native_app/feature/coremodulestest/AppModuleTest.kt
@@ -0,0 +1,47 @@
+package de.tum.informatics.www1.artemis.native_app.feature.coremodulestest
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.SavedStateHandle
+import androidx.work.WorkerParameters
+import de.tum.informatics.www1.artemis.native_app.android.appModule
+import de.tum.informatics.www1.artemis.native_app.core.common.CurrentActivityListener
+import de.tum.informatics.www1.artemis.native_app.core.common.test.UnitTest
+import de.tum.informatics.www1.artemis.native_app.feature.metis.shared.content.StandalonePostId
+import de.tum.informatics.www1.artemis.native_app.feature.quiz.QuizType
+import org.junit.Test
+import org.junit.experimental.categories.Category
+import org.junit.runner.RunWith
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.test.verify.verify
+import org.robolectric.RobolectricTestRunner
+import kotlin.coroutines.CoroutineContext
+
+@OptIn(KoinExperimentalAPI::class)
+@Category(UnitTest::class)
+@RunWith(RobolectricTestRunner::class)
+class AppModuleTest {
+
+    @Test
+    fun checkKoinModule() {
+
+        // Verify Koin configuration
+        appModule.verify(
+            extraTypes = listOf(
+                CoroutineContext::class,
+                SavedStateHandle::class,
+                Boolean::class,
+                Long::class,
+                StandalonePostId::class,
+                Application::class,
+                CurrentActivityListener::class,
+                Context::class,
+                WorkerParameters::class,
+                QuizType.WorkableQuizType::class,
+                QuizType.ViewableQuizType::class,
+            )
+        )
+
+     // TODO: remove old tests
+    }
+}
