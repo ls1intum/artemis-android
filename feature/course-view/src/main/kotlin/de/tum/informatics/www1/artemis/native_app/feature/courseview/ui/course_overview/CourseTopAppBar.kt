570777f8d3400b6c6ca04e2e57b354e7b628d467
Martin Felber <felber.martin3@gmail.com>
2024-12-20 20:22:19 +0100

    Remove tab navigation

--- feature/course-view/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/courseview/ui/course_overview/CourseTopAppBar.kt
+++ feature/course-view/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/courseview/ui/course_overview/CourseTopAppBar.kt
@@ -1,7 +1,6 @@
 package de.tum.informatics.www1.artemis.native_app.feature.courseview.ui.course_overview
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Chat
@@ -13,8 +12,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -93,10 +90,8 @@ fun BottomNavigationBar(
 
 @Composable
 internal fun CourseTopAppBar(
-    selectedTabIndex: Int,
     courseDataState: DataState<Course>,
     scrollBehavior: TopAppBarScrollBehavior,
-    changeTab: (Int) -> Unit,
     onReloadCourse: () -> Unit,
     onNavigateBack: () -> Unit
 ) {
@@ -123,41 +118,5 @@ internal fun CourseTopAppBar(
             },
             scrollBehavior = scrollBehavior
         )
-        TabRow(
-            modifier = Modifier.fillMaxWidth(),
-            selectedTabIndex = selectedTabIndex
-        ) {
-            @Suppress("LocalVariableName")
-            val CourseTab = @Composable { index: Int, text: String, icon: ImageVector ->
-                Tab(
-                    selected = selectedTabIndex == index,
-                    onClick = { changeTab(index) },
-                    text = {
-                        Text(
-                            text = text,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                    },
-                    icon = { Icon(icon, contentDescription = null) }
-                )
-            }
-
-            CourseTab(
-                0,
-                stringResource(id = R.string.course_ui_tab_exercises),
-                Icons.Default.ListAlt
-            )
-            CourseTab(
-                1,
-                stringResource(id = R.string.course_ui_tab_lectures),
-                Icons.Default.School
-            )
-            CourseTab(
-                2,
-                stringResource(id = R.string.course_ui_tab_communication),
-                Icons.Default.Chat
-            )
-        }
     }
 }
