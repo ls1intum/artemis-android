535b3ea07aac14a4c9262536231619d13e2b9cde
Julian Waluschyk <juwalu02@gmail.com>
2025-01-10 14:21:43 +0100

    added new PushNotificationSettingsScreen

---
+++ feature/push/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/push/ui/PushNotificationSettingsScreen.kt
@@ -0,0 +1,164 @@
+package de.tum.informatics.www1.artemis.native_app.feature.push.ui
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import de.tum.informatics.www1.artemis.native_app.core.ui.compose.JobAnimatedFloatingActionButton
+import de.tum.informatics.www1.artemis.native_app.feature.push.R
+import kotlinx.coroutines.Job
+import org.koin.androidx.compose.koinViewModel
+
+/**
+ * Display the notification settings screen.
+ * Contains PushNotificationSettingsUi and is used in the settings screen and after login.
+ */
+@Composable
+fun PushNotificationSettingsScreen(
+    modifier: Modifier = Modifier,
+    isInitialNotificationSettingsScreen: Boolean = false,
+    onDone: () -> Unit
+) {
+    val viewModel: PushNotificationSettingsViewModel = koinViewModel()
+    val isDirty by viewModel.isDirty.collectAsState(initial = false)
+
+    var saveJob: Job? by remember { mutableStateOf(null) }
+    var displaySyncFailedDialog: Boolean by rememberSaveable { mutableStateOf(false) }
+    var displayUnsavedChangesDialog: Boolean by rememberSaveable { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    if (!isInitialNotificationSettingsScreen) {
+                        IconButton(onClick = {
+                            if (isDirty) {
+                                displayUnsavedChangesDialog = true
+                            } else {
+                                onDone()
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                },
+                title = {
+                    if (isInitialNotificationSettingsScreen) {
+                        Text(text = stringResource(id = R.string.initial_push_notification_settings_title))
+                    } else {
+                        Text(text = stringResource(id = R.string.settings_push_notification_settings_screen_title))
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            if (isInitialNotificationSettingsScreen) {
+                ExtendedFloatingActionButton(
+                    onClick = {
+                        // If changes have been made, these need to be synced first.
+                        if (isDirty) {
+                            saveJob = viewModel.saveSettings()
+                            onDone()
+                        } else onDone()
+                    },
+                    text = {
+                        Text(
+                            text = stringResource(
+                                id =
+                                if (isDirty) R.string.initial_push_notification_settings_fab_text_with_save
+                                else R.string.initial_push_notification_settings_fab_text_without_save
+                            )
+                        )
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = if (isDirty) Icons.Default.Save else Icons.AutoMirrored.Filled.ArrowForward,
+                            contentDescription = null
+                        )
+                    }
+                )
+            } else {
+                JobAnimatedFloatingActionButton(
+                    enabled = isDirty,
+                    startJob = { viewModel.saveSettings() },
+                    onJobCompleted = { isSuccessful ->
+                        if (!isSuccessful) {
+                            displaySyncFailedDialog = true
+                        }
+                    }
+                ) {
+                    Icon(imageVector = Icons.Default.Save, contentDescription = null)
+                }
+            }
+        }
+    ) { padding ->
+        PushNotificationSettingsUi(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = padding.calculateTopPadding())
+                .padding(horizontal = 16.dp)
+                .verticalScroll(rememberScrollState())
+                .padding(
+                    bottom = WindowInsets.systemBars
+                        .asPaddingValues()
+                        .calculateBottomPadding()
+                ),
+            viewModel = viewModel
+        )
+
+        if (displaySyncFailedDialog) {
+            PushNotificationSyncFailedDialog {
+                displaySyncFailedDialog = false
+            }
+        }
+
+        if (displayUnsavedChangesDialog) {
+            PushNotificationUnsavedChangesDialog(
+                onDismissRequest = {
+                    displayUnsavedChangesDialog = false
+                    onDone()
+                },
+                onSaveChanges = {
+                    saveJob = viewModel.saveSettings()
+                    onDone()
+                }
+            )
+        }
+
+        if (saveJob != null && isInitialNotificationSettingsScreen) {
+            PushNotificationSyncChangesDialog(
+                onDismissRequest = {
+                    saveJob?.cancel()
+                    saveJob = null
+                }
+            )
+        }
+    }
+}
