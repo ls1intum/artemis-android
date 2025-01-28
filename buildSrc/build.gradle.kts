a04482e0a7986eda577f22438239224d67337594
Martin Felber <45291671+FelberMartin@users.noreply.github.com>
2025-01-28 14:24:38 +0100

    Revert "Bump jvm from 2.1.0 to 2.1.10" (#352)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.10"
 //    id("java-gradle-plugin")
 }
