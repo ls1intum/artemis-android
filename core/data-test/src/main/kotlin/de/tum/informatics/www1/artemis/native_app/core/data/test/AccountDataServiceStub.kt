be965d623d6f300c946e52b94227001381137711
Martin Felber <felber.martin3@gmail.com>
2025-02-12 19:09:30 +0100

    Fix UI test

--- core/data-test/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/data/test/AccountDataServiceStub.kt
+++ core/data-test/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/data/test/AccountDataServiceStub.kt
@@ -4,10 +4,10 @@ import de.tum.informatics.www1.artemis.native_app.core.data.NetworkResponse
 import de.tum.informatics.www1.artemis.native_app.core.data.service.network.AccountDataService
 import de.tum.informatics.www1.artemis.native_app.core.model.account.Account
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
 
 class AccountDataServiceStub(private val account: Account = Account()) : AccountDataService {
-    override val onReloadRequired: Flow<Unit> = emptyFlow()
+    override val onReloadRequired: Flow<Unit> = flowOf(Unit)
 
     override suspend fun getAccountData(): NetworkResponse<Account> = NetworkResponse.Response(account)
 
