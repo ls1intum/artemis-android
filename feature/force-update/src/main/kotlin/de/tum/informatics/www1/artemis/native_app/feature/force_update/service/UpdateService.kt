b6dbd8cb811afd5cd748c1f02c0d63b5188f441c
Eylül Naz Can <eylulnc@gmail.com>
2025-02-20 13:35:10 +0100

    use serverProfileInfoService

--- feature/force-update/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/force_update/service/UpdateService.kt
+++ feature/force-update/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/force_update/service/UpdateService.kt
@@ -1,15 +1,7 @@
 package de.tum.informatics.www1.artemis.native_app.feature.force_update.service
 
 import de.tum.informatics.www1.artemis.native_app.core.data.NetworkResponse
-import de.tum.informatics.www1.artemis.native_app.core.data.cookieAuth
-import de.tum.informatics.www1.artemis.native_app.core.data.performNetworkCall
-import de.tum.informatics.www1.artemis.native_app.core.data.service.KtorProvider
-import de.tum.informatics.www1.artemis.native_app.core.model.server_config.ProfileInfo
-import io.ktor.client.call.body
-import io.ktor.client.request.get
-import io.ktor.http.ContentType
-import io.ktor.http.appendPathSegments
-import io.ktor.http.contentType
+import de.tum.informatics.www1.artemis.native_app.core.datastore.ServerProfileInfoService
 
 interface UpdateService {
     suspend fun getLatestVersion(
@@ -19,27 +11,21 @@ interface UpdateService {
 }
 
 class UpdateServiceImpl(
-    private val ktorProvider: KtorProvider
+    private val serverProfileInfoService: ServerProfileInfoService
 ) : UpdateService {
 
     override suspend fun getLatestVersion(
         serverUrl: String,
         authToken: String
     ): NetworkResponse<String?> {
-        return performNetworkCall {
-            val dto: ProfileInfo = ktorProvider.ktorClient.get(serverUrl) {
-                url {
-                    appendPathSegments( "management", "info")
-                }
-                contentType(ContentType.Application.Json)
-                cookieAuth(authToken)
-            }.body()
-
-            dto.compatibleVersions?.android?.minRequired
+        return when (val response = serverProfileInfoService.getServerProfileInfo(serverUrl)) {
+            is NetworkResponse.Response -> NetworkResponse.Response(response.data.compatibleVersions?.android?.minRequired)
+            is NetworkResponse.Failure -> NetworkResponse.Response(null)
         }
     }
-
 }
+
+
 
 
 
