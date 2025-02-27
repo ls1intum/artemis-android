c94d26decd9e626eae036b98220ce76c737ed444
Martin Felber <felber.martin3@gmail.com>
2025-02-27 10:51:13 +0100

    Add docstring

--- core/data/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/data/service/ArtemisContextBasedService.kt
+++ core/data/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/core/data/service/ArtemisContextBasedService.kt
@@ -13,6 +13,18 @@ interface ArtemisContextBasedService {
     val onReloadRequired: Flow<Unit>
 }
 
+/**
+ * Performs a network call with automatic reloading. The reload happens when the onReloadRequired
+ * flow emits (meaning that the required ArtemisContext for this service changed), or when the
+ * manualReloadFlow emits.
+ *
+ * @param T The type of the response data.
+ * @param S The service type which must implement `ArtemisContextBasedService`.
+ * @param networkStatusProvider Provides the current network status.
+ * @param manualReloadFlow A flow that triggers manual reloads. Defaults to an empty flow.
+ * @param serviceCall A suspend function representing the network call to be performed.
+ * @return A flow emitting `DataState` representing the state of the network call.
+ */
 fun <T : Any, S> S.performAutoReloadingNetworkCall(
     networkStatusProvider: NetworkStatusProvider,
     manualReloadFlow: Flow<Unit> = emptyFlow(),
