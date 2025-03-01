ffeeeab489d742ccb6af9f2f3109fd542b8d8862
Martin Felber <felber.martin3@gmail.com>
2025-03-01 15:49:45 +0100

    feature until "exercise-view" submodule

--- feature/exercise-view/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/exerciseview/service/impl/TextSubmissionServiceImpl.kt
+++ feature/exercise-view/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/exerciseview/service/impl/TextSubmissionServiceImpl.kt
@@ -2,6 +2,7 @@ package de.tum.informatics.www1.artemis.native_app.feature.exerciseview.service.
 
 import de.tum.informatics.www1.artemis.native_app.core.common.artemis_context.ArtemisContextProvider
 import de.tum.informatics.www1.artemis.native_app.core.data.NetworkResponse
+import de.tum.informatics.www1.artemis.native_app.core.data.service.ApiEndpoint
 import de.tum.informatics.www1.artemis.native_app.core.data.service.KtorProvider
 import de.tum.informatics.www1.artemis.native_app.core.data.service.impl.ArtemisContextBasedServiceImpl
 import de.tum.informatics.www1.artemis.native_app.core.model.exercise.submission.Submission
@@ -22,7 +23,7 @@ class TextSubmissionServiceImpl(
         return putRequest {
             url {
                 appendPathSegments(
-                    "api",
+                    *ApiEndpoint.text,
                     "exercises",
                     exerciseId.toString(),
                     "text-submissions"
