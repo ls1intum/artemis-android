a8776108d700578cfb3c7fe08b2120635df2ee3c
Eylül Naz Can <eylulnc@gmail.com>
2025-03-06 00:11:28 +0100

    display lectures and exercise based on time frame

---
+++ feature/course-view/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/courseview/ui/TimeFrameItemsLazyColumn.kt
@@ -0,0 +1,155 @@
+package de.tum.informatics.www1.artemis.native_app.feature.courseview.ui
+
+import android.os.Parcelable
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.SaverScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.snapshots.SnapshotStateMap
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import de.tum.informatics.www1.artemis.native_app.core.ui.Spacings
+import de.tum.informatics.www1.artemis.native_app.feature.courseview.R
+import de.tum.informatics.www1.artemis.native_app.feature.courseview.TimeFrame
+import kotlinx.parcelize.Parcelize
+
+@Composable
+internal fun <T> TimeFrameItemsLazyColumn(
+    modifier: Modifier,
+    timeFrameGroup: List<TimeFrame<T>>,
+    verticalArrangement: Arrangement.Vertical = Arrangement.spacedBy(8.dp),
+    getItemId: T.() -> Long,
+    itemContent: @Composable (Modifier, T) -> Unit
+) {
+    val timeFrameGroupExpandedState: MutableMap<String, Boolean> = rememberSaveable(
+        timeFrameGroup,
+        saver = TimeFrameItemsSectionExpandedSaver
+    ) {
+        SnapshotStateMap<String, Boolean>().apply {
+            timeFrameGroup.forEach { group ->
+                val defaultExpanded = when (group) {
+                    is TimeFrame.Current -> true
+                    is TimeFrame.DueSoon -> true
+                    else -> false
+                }
+                this[group.key] = defaultExpanded
+            }
+        }
+    }
+
+    LazyColumn(
+        modifier = modifier,
+        verticalArrangement = verticalArrangement,
+        contentPadding = Spacings.calculateContentPaddingValues()
+    ) {
+        timeFrameGroup.forEachIndexed { index, group ->
+            item {
+                TimeFrameItemsSectionHeader(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .animateItem()
+                        .clickable {
+                            timeFrameGroupExpandedState[group.key] = timeFrameGroupExpandedState[group.key] != true
+                        }
+                        .padding(horizontal = Spacings.ScreenHorizontalSpacing),
+                    group = group,
+                    expanded = timeFrameGroupExpandedState[group.key] == true,
+                )
+            }
+
+            if (timeFrameGroupExpandedState[group.key] == true) {
+                items(group.items, key = getItemId) { item ->
+                    itemContent(Modifier.animateItem(), item)
+                }
+            }
+
+            if (index < timeFrameGroup.size - 1) {
+                item { HorizontalDivider() }
+            }
+        }
+    }
+}
+
+
+/**
+ * Display a title with the time frame.
+ * Displays an icon button that lets the user expand and collapse the time frame items
+ * @param expanded if the item group this is showing is expanded
+ */
+@Composable
+private fun <T> TimeFrameItemsSectionHeader(
+    modifier: Modifier,
+    group: TimeFrame<T>,
+    expanded: Boolean
+) {
+    val size = group.items.size
+    val groupTitle = when (group) {
+        is TimeFrame.Past<*> -> {
+            "Past (${size})"
+        }
+        is TimeFrame.Current<*> -> {
+            "Current (${size})"
+        }
+        is TimeFrame.Future -> {
+            "Future (${size})"
+        }
+        is TimeFrame.NoDate -> {
+            "No date (${size})"
+        }
+        is TimeFrame.DueSoon -> {
+            "Due soon (${size})"
+        }
+    }
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier.weight(1f),
+            text = groupTitle,
+            style = MaterialTheme.typography.titleMedium
+        )
+
+        val icon = if (expanded) Icons.Default.ExpandLess else Icons.Default.ExpandMore
+        val contentDescription =
+            stringResource(
+                id = if (expanded) R.string.course_ui_list_expand_button_less_content_info
+                else R.string.course_ui_list_expand_button_more_content_info
+            )
+
+        Icon(icon, contentDescription)
+    }
+}
+
+private object TimeFrameItemsSectionExpandedSaver :
+    Saver<SnapshotStateMap<String, Boolean>, TimeFrameItemsSectionExpandedSaver.ParcelWrapper> {
+
+    @Parcelize
+    data class ParcelWrapper(val map: Map<String, Boolean>) : Parcelable
+
+    override fun restore(value: ParcelWrapper): SnapshotStateMap<String, Boolean> {
+        return SnapshotStateMap<String, Boolean>().apply {
+            putAll(value.map)
+        }
+    }
+
+    override fun SaverScope.save(value: SnapshotStateMap<String, Boolean>): ParcelWrapper =
+        ParcelWrapper(value)
+}
