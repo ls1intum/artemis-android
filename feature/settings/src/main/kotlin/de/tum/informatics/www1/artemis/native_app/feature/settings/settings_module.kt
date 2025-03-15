ae2685fbfe52fcd562ae8c148ae5259089e6d1b4
Martin Felber <felber.martin3@gmail.com>
2025-03-15 14:44:07 +0100

    Fix koin

--- feature/settings/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/settings/settings_module.kt
+++ feature/settings/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/settings/settings_module.kt
@@ -1,7 +1,18 @@
 package de.tum.informatics.www1.artemis.native_app.feature.settings
 
+import de.tum.informatics.www1.artemis.native_app.feature.settings.ui.SettingsViewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val settingsModule = module {
-
+    viewModel {
+        SettingsViewModel(
+            accountService = get(),
+            accountDataService = get(),
+            networkStatusProvider = get(),
+            pushNotificationJobService = get(),
+            pushNotificationConfigurationService = get(),
+            appVersionProvider = get()
+        )
+    }
 }
