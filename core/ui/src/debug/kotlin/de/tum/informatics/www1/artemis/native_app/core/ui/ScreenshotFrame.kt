e65706349e0c32f6955ebf3012d6f9037ac3de6f
Martin Felber <felber.martin3@gmail.com>
2025-04-10 22:08:15 +0200

    Improving phone frame

--- core/ui/src/debug/kotlin/de/tum/informatics/www1/artemis/native_app/core/ui/ScreenshotFrame.kt
+++ core/ui/src/debug/kotlin/de/tum/informatics/www1/artemis/native_app/core/ui/ScreenshotFrame.kt
@@ -3,11 +3,17 @@
 package de.tum.informatics.www1.artemis.native_app.core.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,14 +24,33 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.LocalAsyncImagePreviewHandler
 import de.tum.informatics.www1.artemis.native_app.core.ui.remote_images.LocalArtemisImageProvider
 
+val backgroundColor1 = Color(0xff89cff0)
+val backgroundColor2 = Color(0xFF2394C9)
+
+object Phone {
+    val frameWidth = 13.dp
+    const val roundness = 12
+    val backgroundColor = Color.White
+
+    val punchHoleSize = 18.dp
+    val statusBarPadding = 6.dp
+    val statusBarHeight = punchHoleSize + statusBarPadding * 2
+
+    val navigationIndicatorHeight = 5.dp
+    val navigationBarPadding = 6.dp
+    val navigationBarHeight = navigationIndicatorHeight + navigationBarPadding * 2
+}
+
 @Composable
-fun ScreenshotFrame(title: String, content: @Composable () -> Unit) {
+fun ScreenshotFrame(title: String, content: @Composable ColumnScope.() -> Unit) {
     ScreenshotTheme {
         Surface {
             Column(
@@ -33,10 +58,7 @@ fun ScreenshotFrame(title: String, content: @Composable () -> Unit) {
                     .fillMaxSize()
                     .background(
                         Brush.linearGradient(
-                            colors = listOf(
-                                Color(0xff89cff0),
-                                Color(0xFF6FC7F0)
-                            )
+                            colors = listOf(backgroundColor1, backgroundColor2),
                         )
                     )
             ) {
@@ -50,39 +72,120 @@ fun ScreenshotFrame(title: String, content: @Composable () -> Unit) {
                         )
                         .padding(16.dp),
                     text = title,
-                    style = MaterialTheme.typography.headlineLarge,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Medium,
                     textAlign = TextAlign.Center
                 )
 
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .padding(horizontal = 48.dp)
-                            .padding(bottom = 16.dp)
-                            .background(
-                                color = Color.Black,
-                                shape = MaterialTheme.shapes.large
-                            )
+                PhoneFrame {
+                    CompositionLocalProvider(
+                        LocalArtemisImageProvider provides ArtemisImageProviderStub(),
+                        LocalAsyncImagePreviewHandler provides ScreenshotData.Util.configImagePreviewHandler(),
                     ) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(16.dp)
-                        ) {
-                            CompositionLocalProvider(
-                                LocalArtemisImageProvider provides ArtemisImageProviderStub(),
-                                LocalAsyncImagePreviewHandler provides ScreenshotData.Util.configImagePreviewHandler(),
-                            ) {
-                                content()
-                            }
-                        }
+                        content()
                     }
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun ColumnScope.PhoneFrame(
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f)
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 64.dp)
+                .padding(bottom = 16.dp)
+                .border(
+                    width = Phone.frameWidth,
+                    color = Color.Black,
+                    shape = RoundedCornerShape(Phone.roundness)
+                )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(Phone.frameWidth),
+            ) {
+                FakeStatusBar()
+                content()
+                // TODO: inject preview window insets, see https://medium.com/@timo_86166/jetpack-compose-previews-for-edge-to-edge-design-a03b3a3713f3
+
+
+                FakeNavigationBar()
+            }
+        }
+    }
+}
+
+@Composable
+private fun FakeStatusBar() {
+    Box(
+        modifier = Modifier
+            .zIndex(1f)
+
+            .fillMaxWidth()
+            .background(color = Phone.backgroundColor)
+            .padding(vertical = Phone.statusBarPadding)
+    ) {
+        // Punch hole
+        val size = Phone.punchHoleSize
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .background(Color.Black, shape = RoundedCornerShape(50))
+                .height(size)
+                .width(size)
+        )
+    }
+}
+
+@Composable
+private fun FakeNavigationBar() {
+    Box(
+        modifier = Modifier
+            .zIndex(1f)
+            .fillMaxWidth()
+            .background(color = Phone.backgroundColor)
+            .padding(vertical = Phone.navigationBarPadding)
+    ) {
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .background(Color.DarkGray, shape = RoundedCornerShape(50))
+                .height(Phone.navigationIndicatorHeight)
+                .fillMaxWidth(0.25f)
+        )
+    }
+}
+
+
+@Composable
+@PlayStoreScreenshots
+private fun Preview() {
+    ScreenshotFrame(title = "Manage all of your courses in one app") {
+        Box(
+            modifier = Modifier
+                .navigationBarsPadding()
+                .statusBarsPadding()
+                .weight(1f)
+                .background(color = Color(0xFFd0c0f0)),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text(
+                modifier = Modifier
+                    .padding(64.dp),
+                text = "This is a preview of the screenshot frame.",
+                style = MaterialTheme.typography.bodyLarge,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
