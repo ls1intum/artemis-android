cdd1db3e8429006331e82ea6d4a3f174f581e5dc
Martin Felber <felber.martin3@gmail.com>
2025-05-06 17:06:27 +0200

    Design improvement for custom instance screen

--- feature/login/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/login/custom_instance_selection/CustomInstanceSelectionScreen.kt
+++ feature/login/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/login/custom_instance_selection/CustomInstanceSelectionScreen.kt
@@ -3,11 +3,14 @@ package de.tum.informatics.www1.artemis.native_app.feature.login.custom_instance
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DoneOutline
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.ErrorOutline
 import androidx.compose.material.icons.filled.QuestionMark
 import androidx.compose.material3.Button
@@ -21,12 +24,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import de.tum.informatics.www1.artemis.native_app.core.data.NetworkResponse
 import de.tum.informatics.www1.artemis.native_app.core.datastore.ServerConfigurationService
 import de.tum.informatics.www1.artemis.native_app.core.datastore.ServerProfileInfoService
+import de.tum.informatics.www1.artemis.native_app.core.ui.Spacings
 import de.tum.informatics.www1.artemis.native_app.core.ui.markdown.MarkdownText
 import de.tum.informatics.www1.artemis.native_app.feature.login.R
 import kotlinx.coroutines.delay
@@ -94,45 +100,63 @@ internal fun CustomInstanceSelectionScreen(
     val reachStatus: CanReachState = viewModel.isServerReachable.collectAsState().value
 
     Column(
-        modifier = modifier.verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        modifier = modifier
+            .padding(horizontal = Spacings.ScreenHorizontalSpacing)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.weight(1f))
+
         MarkdownText(
+            modifier = Modifier.fillMaxWidth(),
             markdown = stringResource(id = R.string.account_select_custom_instance_selection_instruction),
-            modifier = Modifier.fillMaxWidth()
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.labelLarge
         )
 
-        TextField(
+        Column(
             modifier = Modifier.fillMaxWidth(),
-            value = enteredUrl,
-            onValueChange = viewModel::updateServerUrl,
-            singleLine = true,
-            textStyle = MaterialTheme.typography.bodyLarge,
-            label = { Text(text = stringResource(id = R.string.account_select_custom_instance_selection_text_field_label)) }
-        )
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Icon(
-                imageVector = when (reachStatus) {
-                    CanReachState.UNKNOWN -> Icons.Default.QuestionMark
-                    CanReachState.CONNECTED -> Icons.Default.DoneOutline
-                    CanReachState.FAILED -> Icons.Default.ErrorOutline
-                },
-                contentDescription = null
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = enteredUrl,
+                onValueChange = viewModel::updateServerUrl,
+                singleLine = true,
+                textStyle = MaterialTheme.typography.bodyLarge,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Uri),
+                label = { Text(text = stringResource(id = R.string.account_select_custom_instance_selection_text_field_label)) }
             )
 
-            Text(
-                text = stringResource(
-                    id = when (reachStatus) {
-                        CanReachState.UNKNOWN -> R.string.account_select_custom_instance_selection_reach_unknown
-                        CanReachState.CONNECTED -> R.string.account_select_custom_instance_selection_reach_success
-                        CanReachState.FAILED -> R.string.account_select_custom_instance_selection_reach_failure
-                    }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(
+                    imageVector = when (reachStatus) {
+                        CanReachState.UNKNOWN -> Icons.Default.QuestionMark
+                        CanReachState.CONNECTED -> Icons.Default.Done
+                        CanReachState.FAILED -> Icons.Default.ErrorOutline
+                    },
+                    tint = when (reachStatus) {
+                        CanReachState.UNKNOWN -> MaterialTheme.colorScheme.onSurface
+                        CanReachState.CONNECTED -> MaterialTheme.colorScheme.primary
+                        CanReachState.FAILED -> MaterialTheme.colorScheme.error
+                    },
+                    contentDescription = null
                 )
-            )
+
+                Text(
+                    text = stringResource(
+                        id = when (reachStatus) {
+                            CanReachState.UNKNOWN -> R.string.account_select_custom_instance_selection_reach_unknown
+                            CanReachState.CONNECTED -> R.string.account_select_custom_instance_selection_reach_success
+                            CanReachState.FAILED -> R.string.account_select_custom_instance_selection_reach_failure
+                        }
+                    )
+                )
+            }
         }
 
         Button(
@@ -148,6 +172,8 @@ internal fun CustomInstanceSelectionScreen(
                 text = stringResource(id = R.string.account_select_custom_instance_selection_set_instance_button)
             )
         }
+
+        Spacer(modifier = Modifier.weight(2f))
     }
 }
 
