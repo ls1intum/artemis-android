2b7508823cf47b517bc0137542250eff1dc767d5
Martin Felber <felber.martin3@gmail.com>
2025-05-23 15:02:01 +0200

    Add comment

--- feature/metis/shared/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/metis/shared/content/MetisContext.kt
+++ feature/metis/shared/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/metis/shared/content/MetisContext.kt
@@ -4,10 +4,14 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
- * The context in which metis is used, e.g. in a course, an exercise or personal communication.
+ * Outdated: The context in which metis (communication) is used, e.g. in a course, an exercise or personal communication.
+ * Since quite some time now, metis can only be used in the communication section, and not in other contexts anymore.
+ *
+ * This class should eventually be merged with [ArtemisContext] for an up-to-date context class.
+ * Because this class is deeply used in the codebase, we did not remove it yet.
  */
 @Serializable
-sealed class MetisContext{
+sealed class MetisContext {
     abstract val courseId: Long
 
     @Serializable
