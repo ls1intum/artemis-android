fcc8ff427e2758910335da2aa71a644208a7fccd
Martin Felber <felber.martin3@gmail.com>
2025-05-26 19:28:43 +0200

    Remove pre-2.0 Api paths

--- feature/lecture-view/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/lectureview/LectureUnitAttachmentUtil.kt
+++ feature/lecture-view/src/main/kotlin/de/tum/informatics/www1/artemis/native_app/feature/lectureview/LectureUnitAttachmentUtil.kt
@@ -24,7 +24,7 @@ object LectureUnitAttachmentUtil {
         attachmentLink: String
     ): String {
         return URLBuilder(serverUrl).apply {
-            appendPathSegments(*Api.Core.UploadedFile.path)
+            appendPathSegments(*Api.Core.Files.path)
             appendPathSegments(attachmentLink)
         }.buildString()
     }
