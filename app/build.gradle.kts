120f42bd55235f2121d79804ca9451418d4b6967
Martin Felber <felber.martin3@gmail.com>
2025-07-05 15:01:09 +0200

    Increase version to 2.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 android {
     namespace = "de.tum.informatics.www1.artemis.native_app.android"
 
-    val versionName = "2.1.0"
+    val versionName = "2.1.1"
     val versionCode = 624
 
     setProperty("archivesBaseName", "artemis-android-$versionName-$versionCode")
